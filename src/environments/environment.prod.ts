ca28e5a50d58f802947e1c150acb58a2a04668f1
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-10-28 16:02:49 -0300

    enviroment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiY2tsYWhvcXVlc29kYXgiLCJhIjoiY2sxaWFyb2puMGJlazNnb2Z4b2s1cWx1cyJ9.8jDfVYdQKpico9o-IKj2og'
+  }
 };
